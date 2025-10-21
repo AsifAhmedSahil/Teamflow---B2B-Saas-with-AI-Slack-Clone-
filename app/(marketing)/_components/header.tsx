3f708c6f2dea673cf55b6f407e4bcea4c454c90c
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2025-10-21 22:39:11 +0600

    update

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -2,11 +2,12 @@
 import Link from "next/link";
 
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import React from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Logo from "@/public/logo.png";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 
 const menuItems = [
   { name: "Features", href: "#link" },
@@ -93,7 +94,13 @@ export const HeroHeader = () => {
                 </ul>
               </div>
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
-                <Button
+                <LoginLink className={buttonVariants({
+                  variant: "outline",
+                  size:"sm"
+                })}>
+                  Login 
+                </LoginLink>
+                {/* <Button
                   asChild
                   variant="outline"
                   size="sm"
@@ -102,8 +109,14 @@ export const HeroHeader = () => {
                   <Link href="#">
                     <span>Login</span>
                   </Link>
-                </Button>
-                <Button
+                </Button> */}
+
+                <RegisterLink className={buttonVariants({
+                  size:"sm"
+                })}>
+                  Sign Up
+                </RegisterLink>
+                {/* <Button
                   asChild
                   size="sm"
                   className={cn(isScrolled && "lg:hidden")}
@@ -111,7 +124,7 @@ export const HeroHeader = () => {
                   <Link href="#">
                     <span>Sign Up</span>
                   </Link>
-                </Button>
+                </Button> */}
                 <Button
                   asChild
                   size="sm"
