aeab392ccfd28bedd80da28827137e25e9138ca7
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-11-01 15:28:03 +0600

    create workspace sidebar form:

--- app/(dashboard)/workspace/_components/WorkspaceList.tsx
+++ app/(dashboard)/workspace/_components/WorkspaceList.tsx
@@ -1,46 +1,74 @@
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
 const Workspaces = [
-    {
-        id:"1",
-        name:"TeamFlow",
-        avatar:"TF"
-    },
-    {
-        id:"2",
-        name:"TeamFlow2",
-        avatar:"TF2"
-    },
-    {
-        id:"3",
-        name:"TeamFlow3",
-        avatar:"TF3"
-    },
-    
+  {
+    id: "1",
+    name: "TeamFlow",
+    avatar: "TF",
+  },
+  {
+    id: "2",
+    name: "TeamFlow2",
+    avatar: "TF2",
+  },
+  {
+    id: "3",
+    name: "TeamFlow3",
+    avatar: "TF3",
+  },
+];
+
+const colorCombinations = [
+    'bg-blue-500 hover:bg-blue-600 text-white',
+    'bg-emerald-500 hover:bg-emerald-600 text-white',
+    'bg-purple-500 hover:bg-purple-600 text-white',
+    'bg-amber-500 hover:bg-amber-600 text-white',
+    'bg-rose-500 hover:bg-rose-600 text-white',
+    'bg-indigo-500 hover:bg-indigo-600 text-white',
+    'bg-cyan-500 hover:bg-cyan-600 text-white',
+    'bg-pink-500 hover:bg-pink-600 text-white',
+  
 ]
 
-export function WorkspaceList(){
-    return (
-        <TooltipProvider>
-            <div className="flex flex-col gap-2">
-                {
-                    Workspaces.map((ws) =>(
-                        <Tooltip key={ws.id}>
-                            <TooltipTrigger>
-                                <Button size="icon" className="size-12">
+const getWorkspaceColor = (id:string) =>{
+    const charSum = id.split("").reduce((sum,char) => sum + char.charCodeAt(0),0)
 
-                                <span className="text-sm font-semibold">{ws.avatar}</span>
-                                </Button>
+    const colorIndex = charSum % colorCombinations.length;
 
-                            </TooltipTrigger>
+    return colorCombinations[colorIndex]
+    
+}
 
-                        </Tooltip>
-                    ))
-                }
 
-            </div>
 
-        </TooltipProvider>
-    )
+export function WorkspaceList() {
+  return (
+    <TooltipProvider>
+      <div className="flex flex-col gap-2">
+        {Workspaces.map((ws) => (
+          <Tooltip key={ws.id}>
+            <TooltipTrigger>
+              <Button size="icon"
+            //    className="size-12 transition-all duration-200"
+            className={cn("size-12 transition-all duration-200",getWorkspaceColor(ws.id))}
+               >
+                <span className="text-sm font-semibold cursor-pointer">{ws.avatar}</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+                <p>{ws.name}</p>
+
+            </TooltipContent>
+          </Tooltip>
+        ))}
+      </div>
+    </TooltipProvider>
+  );
 }
--- app/(dashboard)/workspace/layout.tsx
+++ app/(dashboard)/workspace/layout.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from 'react'
 import { WorkspaceList } from './_components/WorkspaceList'
+import CreateWWorkspace from './_components/CreateWWorkspace'
 
 const WorkspaceLayout = ({children}:{children:ReactNode}) => {
   return (
     <div className='flex w-full h-screen '>
         <div className='flex h-full w-16 flex-col items-center bg-secondary py-3 px-2 border-r border-border'>
             <WorkspaceList/>
+            <div className='mt-4 cursor-pointer'>
+              <CreateWWorkspace/>
+            </div>
         </div>
       
     </div>
